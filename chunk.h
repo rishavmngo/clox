2fbbfd17154bed6942eded9e2695f87e0662710b
rishavmngo <rishavinmngo@gmail.com>
2025-01-19 19:04:00 +0530

    added line number for debugging

--- chunk.h
+++ chunk.h
@@ -8,16 +8,18 @@ typedef enum {
   OP_CONSTANT,
   OP_RETURN,
 } Opcode;
+
 typedef struct {
   int count;
   int capacity;
   uint8_t *code;
+  int *lines;
   ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk *chunk);
 void freeChunk(Chunk *chunk);
-void writeChunk(Chunk *chunk, uint8_t byte);
+void writeChunk(Chunk *chunk, uint8_t byte, int line);
 int addConstant(Chunk *chunk, Value value);
 
 #endif // !clox_chunk_h
