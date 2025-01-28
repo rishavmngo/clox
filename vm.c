eac27539d84e4003cfc76251e4f147bcdb68e0f3
rishavmngo <rishavinmngo@gmail.com>
2025-01-28 22:57:16 +0530

    added hash table and intern strings with it

--- vm.c
+++ vm.c
@@ -5,6 +5,7 @@
 #include "debug.h"
 #include "memory.h"
 #include "object.h"
+#include "table.h"
 
 #include <stdarg.h>
 #include <stdio.h>
@@ -28,8 +29,12 @@ static void runtimeError(const char *format, ...) {
 void initVM() {
   resetStack();
   vm.objects = NULL;
+  initTable(&vm.strings);
 }
-void freeVM() { freeObjects(); }
+void freeVM() {
+  freeTable(&vm.strings);
+  freeObjects();
+}
 void push(Value value) {
   *vm.stackTop = value;
   vm.stackTop++;
